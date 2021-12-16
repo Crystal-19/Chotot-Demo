5d97e72472a37d24ac2f600ec1f7b04f59c3ece0
Crystal0997 <trankimngan190997@gmail.com>
2021-12-16 17:53:24 +0700

    fix project structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+// import { Routes, Route, Link } from 'react-router-dom'
 
-import Homepgae from 'pages/Home'
-import Newspgae from 'pages/News'
-import Contactpgae from 'pages/Contact'
-
-import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to='/' > Home </Link>
-          </li>
-          <li>
-            <Link to='/news' > News </Link>
-          </li>
-          <li>
-            <Link to='/contact' > Contact </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path='/' element={ < Homepgae />} />
-        <Route path='/news' element={ < Newspgae />} />
-        <Route path='/contact' element={ < Contactpgae />} />
-      </Routes>
-    </div>
+    <div></div>
   )
 }
 
