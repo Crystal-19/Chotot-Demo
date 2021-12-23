0e65134a83e16d940aabc38534f5a0e7da3b2fc7
Tran Kim Ngan <ella@inMacs-MacBook-Pro.local>
2021-12-23 22:20:43 +0700

    fix feedback of code styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,57 +11,69 @@ import {ReactComponent as RegisterIcon} from 'assets/images/icons/register.svg'
 
 import './styles.scss'
 
-const MainHeader = () => {
-  return (
-    <>
-      <div className="general-container header-container">
+const Header = () => {
+  const aboveHeader = () => {
+    return (
+      <div className="header-container">
         <div className="logo-container">
           <Image
-            className='logo-img'
+            className="logo-img"
             src="https://static.chotot.com/storage/default/transparent_logo.webp"
             size="tiny"
-            href='https://www.chotot.com/'
+            href="https://www.chotot.com/"
           />
         </div>
         <div className="items-container">
-          <div className="item item-hide">
-            <HomeIcon className=" iconhome"/>
+          <a href="/" className="item item-hide">
+            <HomeIcon className=" iconhome" />
             <span> Home Page </span>
-          </div>
-          <div className="item item-hide">
+          </a>
+          <a href="/news" className="item item-hide">
             <NewsIcon className="iconnews" />
             <span>News Management</span>
-          </div>
-          <div className="item">
+          </a>
+          <a href="/chat" className="item">
             <ChatIcon className="iconchat" />
-            <span className='item-hide'>Chat</span>
-          </div>
-          <div className="item item-hide">
+            <span className="item-hide">Chat</span>
+          </a>
+          <a href="/notify" className="item item-hide">
             <NotiIcon className="iconnoti" />
             <span>Notify</span>
-          </div>
-          <div className="item item-hide">
+          </a>
+          <a href="/more" className="item item-hide">
             <MoreIcon className="iconmore" />
             <span>More</span>
-          </div>
+          </a>
         </div>
       </div>
-      <div className='general-container header-container' id='searchbar'>
-        <div className='searchbar'>
-          <Input icon='search' placeholder='Search on Cho Tot' />
+    )
+  }
+
+  const belowHeader = () => {
+    return (
+      <div className="header-container header-searchbar">
+        <div className="searchbar">
+          <Input icon="search" placeholder="Search on Cho Tot" />
         </div>
-        <div className='log'>
+        <button className="log">
           <LogInIcon className="log-i" />
           <span> Log in</span>
-        </div>
-        <button className='reg-btn'>
-          <div className='reg'>
+        </button>
+        <button className="reg-btn">
+          <div className="reg">
             <RegisterIcon className="reg-i" />
             <span> Register </span>
           </div>
         </button>
       </div>
-    </>
+    )
+  }
+
+  return (
+    <header className='general-container'>
+      {aboveHeader()}
+      {belowHeader()}
+    </header>
   )
 }
-export default MainHeader
+export default Header
