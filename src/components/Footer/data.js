c96f82b56214a3dfcd547012c2b476dd37182b54
Tran Kim Ngan <ella@inMacs-MacBook-Pro.local>
2021-12-30 10:16:47 +0700

    fix redundant indents

--- src/components/Footer/data.js
+++ src/components/Footer/data.js
@@ -1,85 +1,85 @@
 export const socialData = [
   {
     href: 'https://www.facebook.com/chotot.vn',
-    src: 'https://static.chotot.com/storage/default/facebook.svg'
+    src: 'https://static.chotot.com/storage/default/facebook.svg',
   },
   {
     href: 'https://www.youtube.com/user/ChototVN',
-    src: 'https://static.chotot.com/storage/default/youtube.svg'
+    src: 'https://static.chotot.com/storage/default/youtube.svg',
   },
   {
     href: 'https://plus.google.com/112159203362880782068',
-    src: 'https://static.chotot.com/storage/default/google.svg'
-  }
+    src: 'https://static.chotot.com/storage/default/google.svg',
+  },
 ]
 
-export const appItems =[
+export const appItems = [
   {
     href: 'https://apps.apple.com/us/app/chotot-vn/id790034666',
-    src: 'https://static.chotot.com/storage/default/ios.svg'
+    src: 'https://static.chotot.com/storage/default/ios.svg',
   },
   {
     href: 'https://play.google.com/store/apps/details?id=com.chotot.vn',
-    src: 'https://static.chotot.com/storage/default/android.svg'
+    src: 'https://static.chotot.com/storage/default/android.svg',
   },
   {
     href: 'https://static.chotot.com/storage/default/huawei_app_install.webp',
-    src: 'https://static.chotot.com/storage/default/huawei_app_install.png'
-  }
+    src: 'https://static.chotot.com/storage/default/huawei_app_install.png',
+  },
 ]
 export const customerItems = [
   {
     href: 'https://trogiup.chotot.com/?_ga=2.250313408.751718731.1640081374-451603730.1639971106',
-    text: 'Help Center'
+    text: 'Help Center',
   },
   {
     href: 'https://trogiup.chotot.com/nguoi-mua/cho-tot-bao-ve-nguoi-dung-nhu-the-nao/?_ga=2.53286510.751718731.1640081374-451603730.1639971106',
-    text: 'Safe buying and selling'
+    text: 'Safe buying and selling',
   },
   {
     href: 'https://trogiup.chotot.com/nguoi-mua/cho-tot-bao-ve-nguoi-dung-nhu-the-nao/?_ga=2.53286510.751718731.1640081374-451603730.1639971106',
-    text: 'Rules to know'
+    text: 'Rules to know',
   },
   {
     href: 'https://trogiup.chotot.com/nguoi-ban/rieng-tu/?_ga=2.53286510.751718731.1640081374-451603730.1639971106',
-    text: 'Privacy Policy'
+    text: 'Privacy Policy',
   },
   {
     href: 'https://trogiup.chotot.com/lien-he/?_ga=2.86185566.751718731.1640081374-451603730.1639971106',
-    text: 'contact help'
-  }
+    text: 'contact help',
+  },
 ]
 
 export const marketItems = [
   {
     href: 'https://trogiup.chotot.com/nguoi-ban/gioi-thieu-chotot-com/?_ga=2.86185566.751718731.1640081374-451603730.1639971106',
-    text: 'Introduce'
+    text: 'Introduce',
   },
   {
     href: 'https://careers.chotot.com/?_ga=2.14966768.751718731.1640081374-451603730.1639971106',
-    text: 'Recuitment'
+    text: 'Recuitment',
   },
   {
     href: 'https://truyenthong.chotot.com/?_ga=2.233324190.1214802101.1640660618-451603730.1639971106',
-    text: 'The media'
+    text: 'The media',
   },
   {
     href: 'https://www.chotot.com/kinh-nghiem/',
-    text: 'Blog'
+    text: 'Blog',
   },
 ]
- export const textFooter = [
-   {
-     text: `CHINESE COMPANY LIMITED - Address: Room 1808, Floor 18, Me Linh Point
-     Tower, 02 Ngo Duc Ke, Ben Nghe Ward, District 1, Ho Chi Minh City`
-   },
-   {
+export const textFooter = [
+  {
+    text: `CHINESE COMPANY LIMITED - Address: Room 1808, Floor 18, Me Linh Point
+    Tower, 02 Ngo Duc Ke, Ben Nghe Ward, District 1, Ho Chi Minh City`,
+  },
+  {
     text: `Business Registration Certificate No. 0312120782 issued by the
     Department of Planning and Investment Ho Chi Minh City Investment
-    issued on 11/01/2013`
+    issued on 11/01/2013`,
   },
   {
     text: `Email: trogiup@chotot.vn - Customer Service Call Center: 19003003
-    (1,000 VND/minute)`
-  }
- ]
+    (1,000 VND/minute)`,
+  },
+]
