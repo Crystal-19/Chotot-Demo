125d1a7c9a39147f444456f9b62f17ef60f20a7b
Tran Kim Ngan <ella@inMacs-MacBook-Pro.local>
2022-01-04 18:48:43 +0700

    add the bottom bar of the slider

--- src/pages/Home/components/Slider/index.js
+++ src/pages/Home/components/Slider/index.js
@@ -3,19 +3,29 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import {Carousel} from 'react-responsive-carousel'
 
 import {Image} from 'semantic-ui-react'
-import {SliderImage} from './data'
+import {SliderImage, SliderBottomBar} from './data'
 
 import './styles.scss'
 
 const Slider = () => {
   return (
-    <Carousel infiniteLoop className="general-container">
-      {SliderImage.map((dt, index) => (
-        <div className="slider-container " key={index}>
-          <Image className="slider-image" src={dt.src} href={dt.url} />
-        </div>
-      ))}
-    </Carousel>
+    <nav className="general-container">
+      <Carousel infiniteLoop>
+        {SliderImage.map((dt, index) => (
+          <div className="slider-container " key={index}>
+            <Image className="slider-image" src={dt.src} href={dt.url} />
+          </div>
+        ))}
+      </Carousel>
+      <div className="slider-bar-container">
+        {SliderBottomBar.map((dt, index) => (
+          <div key={index} className="items-container">
+            <Image className="icon-image" src={dt.src} href={dt.url} />
+            <span>{dt.content}</span>
+          </div>
+        ))}
+      </div>
+    </nav>
   )
 }
 export default Slider
