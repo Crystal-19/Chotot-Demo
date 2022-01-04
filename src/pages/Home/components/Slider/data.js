b33de14c87019e560bb591b598fbbaf848fc95f3
Tran Kim Ngan <ella@inMacs-MacBook-Pro.local>
2022-01-04 17:22:09 +0700

    build the slider in home page

---
+++ src/pages/Home/components/Slider/data.js
@@ -0,0 +1,34 @@
+export const SliderImage = [
+  {
+    src: 'https://cdn.chotot.com/admincentre/1cKK_EF5aLKANWaBMAnStDkzZhhOEe8HHJ6A2rjgzjA/preset:raw/plain/b43a8b89ffa6b7d80f0ab85fec43adfd-2752611356625446359.jpg',
+    url: 'https://chuongtrinh.chotot.com/mua-hang-khong-dan-do-voi-ma-ship-1k-tu-cho-tot-ahamove/'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/rNNLJVkI76mmU5eCUKsISHxkuMYF1VaGZbpyaTdwRQ8/preset:raw/plain/87ee52c551bbd1bde383e4aa4859e82b-2751765820463493997.jpg',
+    url: 'https://nha.chotot.com/kinh-nghiem/thi-truong-bat-dong-san-quy-4-2021.html'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/_YlNA__9ByLKg0UChc7w8DNBKoIYVeoQ0DD_hRYq4TI/preset:raw/plain/db18c052a2f1b235c7361046df243226-2751275551476831103.jpg',
+    url: 'https://www.vieclamtot.com/kinh-nghiem/bao-cao-viec-lam-tot.html'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/JHJNXdyL8E0xP5Nx4uZfN74goDYK8oiDlxQaa6AB99g/preset:raw/plain/9464351bbae0a5136d9130b21df786c1-2752018487287985036.jpg',
+    url: 'https://nha.chotot.com/tags/can-ho-chung-cu-tp-thu-duc'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/7_akiLTZ-C9qtYyGT4emFLzASvYYc11b1C-2m8pXL30/preset:raw/plain/2f7385af6fca39f105bd840c1807fed4-2752500050281976686.jpg',
+    url: 'https://www.chotot.com/chuong-trinh/sam-chuan-xe-moi'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/_ErJvYbw7MiTN2Ao9z4k2wQ7iq6wY2aUeaVnNYN6FqI/preset:raw/plain/ddb559daa6f647e05ffe98e50e92f582-2749115625309801416.jpg',
+    url: 'https://www.chotot.com/chuong-trinh/san-hang-like-new'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/juAIXwj8i2Lfz3IqqN1b7FyqPwOHX8FBw8sWOY9Q-Fo/preset:raw/plain/2fa35a7c329947762c80a26376925375-2751554487573513608.jpg',
+    url: 'https://www.chotot.com/chuong-trinh/vong-quay-may-man/web/game'
+  },
+  {
+    src: 'https://cdn.chotot.com/admincentre/CV9DgHAzZhYRsbKZzHKb77vPrwySqKxUSdXgLS7sjVI/preset:raw/plain/a6c8916c5bb9165c4b8e947666f1ded2-2749962151746793802.jpg',
+    url: 'https://www.chotot.com/chuong-trinh/bat-dong-san-thu-duc'
+  }
+]
--- src/pages/Home/components/Slider/index.js
+++ src/pages/Home/components/Slider/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import {Carousel} from 'react-responsive-carousel'
+
+import {Image} from 'semantic-ui-react'
+import {SliderImage} from './data'
+
+import './styles.scss'
 
 const Slider = () => {
   return (
-    <div>
-      Slider
-    </div>
+    <Carousel infiniteLoop className="general-container">
+      {SliderImage.map((dt, index) => (
+        <div className="slider-container " key={index}>
+          <Image className="slider-image" src={dt.src} href={dt.url} />
+        </div>
+      ))}
+    </Carousel>
   )
 }
 export default Slider
