bf30e43edd7f6d52981ab4ed3b653d3ddac202af
Ella <trankimngan190997@gmail.com>
2022-01-18 22:38:38 +0700

    handle to show more product when click see more

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Image, Icon } from 'semantic-ui-react'
-import {data} from './data'
+import { data } from './data'
 
 import './styles.scss'
 
 const Product = () => {
+    const [limit, setLimit] = useState(20)
+    let dataShow = data.filter((dt, index) => index < limit)
+
+    const onShowMore = () => {
+        setLimit(limit+10)
+        dataShow = data.filter((dt, index) => index < limit)
+    }
+
   return (
     <div className="general-container container">
       <h3>New Post</h3>
       <div className="products-container">
-        {data.map(dt => (
+        {dataShow.map(dt => (
           <div key={dt.id} className="product-container">
             <Image className="product-img" src={dt.src} />
             <p>{dt.info}</p>
@@ -22,9 +30,9 @@ const Product = () => {
           </div>
         ))}
       </div>
-      <div className="see-more">
-          <p>see more</p>
-          <Icon name='angle down' />
+      <div onClick={onShowMore} className="see-more">
+        <p>see more</p>
+        <Icon name="angle down" />
       </div>
     </div>
   )
