72147260a39593e2ce413a953d56382d3d80d4a3
Ella <trankimngan190997@gmail.com>
2022-01-18 21:05:42 +0700

    UI of product

---
+++ src/components/Product/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Image, Icon, Divider } from 'semantic-ui-react'
+import { data } from './data'
+
+import './styles.scss'
+
+const Product = () => {
+    return (
+        <div className='general-container container'>
+            <h3>New Post</h3>
+            <div  className="products-container">
+            {data.map(dt => (
+                    <div key={dt.id} className="product-container">
+                        <Image className='product-img' src={dt.src} />
+                        <p>{dt.info}</p>
+                        <p className='price'>{dt.price}</p>
+                        <div className="address">
+                            <Icon name={dt.icon} />
+                            <span>{dt.address}</span>
+                        </div>
+                        
+                    </div>
+                    
+            ))}
+            </div>
+        </div>
+    )
+}
+export default Product
