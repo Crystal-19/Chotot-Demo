8e6894789cb185f1abb54f9acef2c2708f8e701b
Ella <trankimngan190997@gmail.com>
2022-01-18 12:58:26 +0700

    responsive tablet and mobile

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -9,13 +9,15 @@ const Catalog = () => {
   return (
     <div className="general-container container">
       <h3>Explore categories</h3>
-      <div className="catalog-container">
-        {data.map(dt => (
-          <div key={dt.id} className="items-container">
-            <Image className="catalog-img" src={dt.src} href={dt.href} />
-            <p>{dt.catalog}</p>
-          </div>
-        ))}
+      <div className="scroll-container">
+        <div className="catalog-container scroll-container">
+          {data.map(dt => (
+            <div key={dt.id} className="items-container">
+              <Image className="catalog-img" src={dt.src} href={dt.href} />
+              <p>{dt.catalog}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
