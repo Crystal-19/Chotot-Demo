67ce9374df7f179016530ffa21d6cca3dda4aacf
Ella <trankimngan190997@gmail.com>
2022-01-21 11:43:42 +0700

    add id into route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="/news" element={<News />} />
-          <Route path="/product" element={<ProductDetail />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
