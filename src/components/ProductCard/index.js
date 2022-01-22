5ff61b6a50ab45f000f34c8aa82c8508202bb8ad
Ella <trankimngan190997@gmail.com>
2022-01-22 18:02:52 +0700

    remove import useParams

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Link, useParams} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {Image, Icon} from 'semantic-ui-react'
 
 import './styles.scss'
