1253e2e04fc2d7f8255b91e285049fec122bbb04
Ella <trankimngan190997@gmail.com>
2022-02-24 14:22:11 +0700

    using dayjs to show date time

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 import {Link} from 'react-router-dom'
 import {Image, Icon} from 'semantic-ui-react'
@@ -7,9 +9,13 @@ import * as helpers from 'utils/helpers'
 
 import './styles.scss'
 
+dayjs.extend(relativeTime)
+
 const ProductCard = ({product}) => {
   const {_id, imageUrl, name, price, icon, location, createdAt} = product
-  
+
+  const currentTime = dayjs(createdAt).fromNow()
+
   return (
     <Link to={`/product/${_id}`} className="product-container">
       <Image className="product-img" src={imageUrl} />
@@ -17,7 +23,7 @@ const ProductCard = ({product}) => {
       <p className="price">{helpers.formatPrice(price)}</p>
       <div className="location">
         <Icon name={icon} />
-        <span>1 minute ago</span>
+        <span>{currentTime}</span>
         <span>{location}</span>
       </div>
     </Link>
