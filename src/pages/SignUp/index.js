c7697493913281ebd43704d161224cfd1252bbfb
Ella <trankimngan190997@gmail.com>
2022-03-15 12:26:52 +0700

    copy content from logsign component to signup component

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,26 +1,54 @@
 import React from 'react'
 
-import LogSign from 'components/LogSign'
+import {Image} from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+import Footer from 'components/Footer'
+
+import './styles.scss'
+
+const renderSignupTitle = () => {
+  return (
+    <div className="signup-title">
+      <div className="signup-text">
+        <h2>Registration</h2>
+        <h5>Create a Cho Tot account now</h5>
+      </div>
+      <Image
+        src="https://static.chotot.com/storage/assets/LOGIN/logo.svg"
+        className="chotot-img"
+      />
+    </div>
+  )
+}
+
+const renderInputSection = () => {
+  return (
+    <>
+      <input placeholder="Enter your email" />
+      <input
+        placeholder="Create a password with at lease 6 characters"
+        type="password"
+      />
+      <button>Registration</button>
+      <div className="register-container">
+        <p>Do you already have an account?</p>
+        <Link to="/login">Log in</Link>
+      </div>
+    </>
+  )
+}
 
 const SignUp = () => {
-  const title = 'Registration'
-  const content = 'Create a Cho Tot account now'
-  const password = 'Create a password with at lease 5 characters'
-  const buttonContent = 'Registration'
-  const text = 'Do you already have an account?'
-  const path = '/login'
-  const link = 'Log in'
- 
   return (
-    <LogSign 
-      title={title}
-      content={content}
-      password={password}
-      buttonContent={buttonContent}
-      text={text}
-      path={path}
-      link={link}
-    />
+    <div className="background-img">
+      <div className="general-container structure-container">
+        <div className="signup-container">
+          {renderSignupTitle()}
+          {renderInputSection()}
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 export default SignUp
