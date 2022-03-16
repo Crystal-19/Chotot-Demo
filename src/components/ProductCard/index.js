431da113c291fd7c60c8b0248dd0e3206ff3e0c3
Ella <trankimngan190997@gmail.com>
2022-03-16 16:34:33 +0700

    add three dot into product card

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -10,23 +10,28 @@ import * as helpers from 'utils/helpers'
 import './styles.scss'
 
 dayjs.extend(relativeTime)
-
+const test = () => {
+  console.log('test')
+}
 const ProductCard = ({product}) => {
   const {_id, imageUrl, name, price, icon, location, createdAt} = product
 
   const currentTime = dayjs(createdAt).fromNow()
 
   return (
-    <Link to={`/product/${_id}`} className="product-container">
-      <Image className="product-img" src={imageUrl} />
-      <p>{name}</p>
+    <div className="product-container">
+      <Icon name="ellipsis vertical" onClick={test} />
+      <Link to={`/product/${_id}`}>
+        <Image className="product-img" src={imageUrl} />
+        <p>{name}</p>
+      </Link>
       <p className="price">{helpers.formatPrice(price)}</p>
       <div className="location">
         <Icon name={icon} />
         <span>{currentTime}</span>
         <span>{location}</span>
       </div>
-    </Link>
+    </div>
   )
 }
 export default ProductCard
