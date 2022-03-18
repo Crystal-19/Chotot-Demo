9b3986fe83b7ed60479afc050b82825da14a3805
Ella <trankimngan190997@gmail.com>
2022-03-18 20:51:03 +0700

    add modal

--- src/components/CreateProduct/index.js
+++ src/components/CreateProduct/index.js
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import {Dropdown, Menu} from 'semantic-ui-react'
+import {Dropdown, Menu, Modal, Icon, Image} from 'semantic-ui-react'
 import {ReactComponent as CameraIcon} from 'assets/images/icons/camera.svg'
 import {ReactComponent as Plus} from 'assets/images/icons/plus.svg'
 
 import './styles.scss'
 
 const CreateProduct = () => {
+  const [open, setOpen] = useState(true)
+
   const categories = [
-    {key: 1, text: 'Pets', value: 1},
-    {key: 2, text: 'Vehicles', value: 2},
-    {key: 3, text: 'Food', value: 3},
-    {key: 3, text: 'Electronice Devices', value: 4},
-    {key: 3, text: 'Real Estate', value: 5},
-    {key: 3, text: 'Furnitures', value: 6},
-    {key: 3, text: 'Travel Services', value: 7},
+    {key: 1, category: 'Pets', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/12000.png'},
+    {key: 2, category: 'Vehicles', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/2000.png'},
+    {key: 3, category: 'Food', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/7000.png'},
+    {key: 4, category: 'Electronic Devices', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/5000.png'},
+    {key: 5, category: 'Real Estate', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/1000.png'},
+    {key: 6, category: 'Furniture', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/14000.png'},
+    {key: 7, category: 'Travel Services', imageUrl: 'https://static.chotot.com/storage/categories/all-category-v3/6000.png'},
   ]
+
+  const renderModal = () => {
+    return (
+      <Modal
+        centered={false}
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}>
+        <Modal.Header>
+          <Modal.Actions>
+            <Icon onClick={() => setOpen(false)} name='times circle outline' />
+          </Modal.Actions>{' '}
+          Select a post category
+        </Modal.Header>
+        <Modal.Content>
+          {categories.map(cd => (
+            <Modal.Description key={cd.key}>
+              <Image src={cd.imageUrl} />
+              <h2>{cd.category}</h2>
+              <Icon name="chevron right" />
+            </Modal.Description>
+          ))}
+        </Modal.Content>
+      </Modal>
+    )
+  }
 
   const renderImageUpload = () => {
     return (
@@ -45,14 +73,19 @@ const CreateProduct = () => {
       </div>
     )
   }
-  
+
   const renderForm = () => {
     return (
       <div className="product-info-container">
         <form>
           <Menu vertical>
             <Menu.Item header>List Of Posts</Menu.Item>
-            <Dropdown text="Dropdown" simple item />
+            <Dropdown
+              onClick={() => setOpen(!open)}
+              text="Dropdown"
+              simple
+              item
+            />
           </Menu>
           <h2>Details</h2>
           {renderInput('name', 'text')}
@@ -71,6 +104,7 @@ const CreateProduct = () => {
   return (
     <div className="background-container">
       <div className="create-product-container general-container">
+        {renderModal()}
         {renderImageUpload()}
         {renderForm()}
       </div>
