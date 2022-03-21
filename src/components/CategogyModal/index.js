a74a585b9aa7c4d181027b0d10e8dedfd89b4e37
Ella <trankimngan190997@gmail.com>
2022-03-21 13:55:18 +0700

    rewrite modal as a componenet

---
+++ src/components/CategogyModal/index.js
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react'
+
+import {useSelector, useDispatch} from 'react-redux'
+import {Modal, Icon, Image} from 'semantic-ui-react'
+
+import * as categoryActions from 'redux/actions/categoryActions'
+
+const CategoryModal = ({open, setOpen}) => {
+  const dispatch = useDispatch()
+  const categories = useSelector(state => state.Category.category)
+
+  useEffect(() => {
+    dispatch(categoryActions.loadCategory())
+  }, [dispatch])
+
+  return (
+    <Modal
+        centered={false}
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}>
+        <Modal.Header>
+          <Modal.Actions>
+            <Icon onClick={() => setOpen(false)} name="times circle outline" />
+          </Modal.Actions>{' '}
+          Select a post category
+        </Modal.Header>
+        <Modal.Content>
+          {categories.map(cd => (
+            <Modal.Description key={cd._id}>
+              <Image src={cd.imageUrl} />
+              <h2>{cd.name}</h2>
+              <Icon name="chevron right" />
+            </Modal.Description>
+          ))}
+        </Modal.Content>
+      </Modal>
+  )
+}
+export default CategoryModal
--- src/pages/CreateProduct/index.js
+++ src/pages/CreateProduct/index.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react'
 
-import {Dropdown, Menu, Modal, Icon, Image, Button} from 'semantic-ui-react'
+import {Dropdown, Menu, Icon, Image, Button} from 'semantic-ui-react'
 import {ReactComponent as CameraIcon} from 'assets/images/icons/camera.svg'
 import {ReactComponent as Plus} from 'assets/images/icons/plus.svg'
 import FloatLabelInput from 'components/FloatLabelInput'
 import PreviewProduct from 'components/PreviewProduct'
+import CategoryModal from 'components/CategogyModal'
 
 import './styles.scss'
 
@@ -13,50 +14,6 @@ const CreateProduct = () => {
   const [imageUpload, setImageUpload] = useState(null)
   const [preview, setPreview] = useState(false)
 
-  const categories = [
-    {
-      key: 1,
-      category: 'Pets',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/12000.png',
-    },
-    {
-      key: 2,
-      category: 'Vehicles',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/2000.png',
-    },
-    {
-      key: 3,
-      category: 'Food',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/7000.png',
-    },
-    {
-      key: 4,
-      category: 'Electronic Devices',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/5000.png',
-    },
-    {
-      key: 5,
-      category: 'Real Estate',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/1000.png',
-    },
-    {
-      key: 6,
-      category: 'Furniture',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/14000.png',
-    },
-    {
-      key: 7,
-      category: 'Travel Services',
-      imageUrl:
-        'https://static.chotot.com/storage/categories/all-category-v3/6000.png',
-    },
-  ]
 
   const handleImageUpload = e => {
     setImageUpload(URL.createObjectURL(e.target.files[0]))
@@ -68,32 +25,6 @@ const CreateProduct = () => {
 
   const handlePreview = () => {
     setPreview(!preview)
-  }
-
-  const renderModal = () => {
-    return (
-      <Modal
-        centered={false}
-        open={open}
-        onClose={() => setOpen(false)}
-        onOpen={() => setOpen(true)}>
-        <Modal.Header>
-          <Modal.Actions>
-            <Icon onClick={() => setOpen(false)} name="times circle outline" />
-          </Modal.Actions>{' '}
-          Select a post category
-        </Modal.Header>
-        <Modal.Content>
-          {categories.map(cd => (
-            <Modal.Description key={cd.key}>
-              <Image src={cd.imageUrl} />
-              <h2>{cd.category}</h2>
-              <Icon name="chevron right" />
-            </Modal.Description>
-          ))}
-        </Modal.Content>
-      </Modal>
-    )
   }
 
   const renderImageUpload = () => {
@@ -165,7 +96,7 @@ const CreateProduct = () => {
   const renderCreateProduct = () => {
     return (
       <div className="create-product-container general-container">
-        {renderModal()}
+        <CategoryModal open={open} setOpen={setOpen} />
         {imageUpload === null ? renderImageUpload() : renderUploadedImage()}
         {renderForm()}
       </div>
