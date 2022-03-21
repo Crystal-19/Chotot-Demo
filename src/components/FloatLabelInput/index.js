d750465a7966cb64abc8e849bd5941e1bbd0075e
Ella <trankimngan190997@gmail.com>
2022-03-21 10:00:00 +0700

    placeholder = hide to understand more easily

--- src/components/FloatLabelInput/index.js
+++ src/components/FloatLabelInput/index.js
@@ -5,7 +5,7 @@ import './styles.scss'
 const Input = ({id, type}) => {
   return (
     <div className="input-container">
-      <input id={id} type={type} placeholder={id} required />
+      <input id={id} type={type} placeholder='hide' required />
       <label htmlFor={id}>{id}</label>
     </div>
   )
