0fcd5ec617887a8c3e9b063352aadde7ac4632b6
Ella <trankimngan190997@gmail.com>
2022-03-21 14:11:51 +0700

    re-stylying modal

--- src/components/CategogyModal/index.js
+++ src/components/CategogyModal/index.js
@@ -5,6 +5,8 @@ import {Modal, Icon, Image} from 'semantic-ui-react'
 
 import * as categoryActions from 'redux/actions/categoryActions'
 
+import './styles.scss'
+
 const CategoryModal = ({open, setOpen}) => {
   const dispatch = useDispatch()
   const categories = useSelector(state => state.Category.category)
@@ -15,26 +17,26 @@ const CategoryModal = ({open, setOpen}) => {
 
   return (
     <Modal
-        centered={false}
-        open={open}
-        onClose={() => setOpen(false)}
-        onOpen={() => setOpen(true)}>
-        <Modal.Header>
-          <Modal.Actions>
-            <Icon onClick={() => setOpen(false)} name="times circle outline" />
-          </Modal.Actions>{' '}
-          Select a post category
-        </Modal.Header>
-        <Modal.Content>
-          {categories.map(cd => (
-            <Modal.Description key={cd._id}>
-              <Image src={cd.imageUrl} />
-              <h2>{cd.name}</h2>
-              <Icon name="chevron right" />
-            </Modal.Description>
-          ))}
-        </Modal.Content>
-      </Modal>
+      centered={false}
+      open={open}
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}>
+      <Modal.Header>
+        <Modal.Actions>
+          <Icon onClick={() => setOpen(false)} name="times circle outline" />
+        </Modal.Actions>{' '}
+        Select a post category
+      </Modal.Header>
+      <Modal.Content>
+        {categories.map(cd => (
+          <Modal.Description key={cd._id}>
+            <Image src={cd.imageUrl} />
+            <h2>{cd.name}</h2>
+            <Icon name="chevron right" />
+          </Modal.Description>
+        ))}
+      </Modal.Content>
+    </Modal>
   )
 }
 export default CategoryModal
