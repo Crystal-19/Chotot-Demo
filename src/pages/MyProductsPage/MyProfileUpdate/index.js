ee48a0ceed1570c9657322d771d272088695c872
Ella <trankimngan190997@gmail.com>
2022-03-24 12:15:01 +0700

    add input type file into image update

--- src/pages/MyProductsPage/MyProfileUpdate/index.js
+++ src/pages/MyProductsPage/MyProfileUpdate/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {useSelector} from 'react-redux'
-import {Image} from 'semantic-ui-react'
+import {Image, Icon} from 'semantic-ui-react'
 
 import Breadcrumb from 'components/Breadcrumb'
 import FloatLabelInput from 'components/FloatLabelInput'
@@ -25,13 +25,14 @@ const MyProfileUpdate = () => {
         <h2>Personal Information</h2>
         <div className="profile-edit-container">
           <div className="img-edit-container">
+            <input accept="image/*" type="file" className="img-input-update" />
             <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlVuC9HTggrhVG9Nr-djhhRPNAoGYwkUcpZxwk8yXFxtW6yUqSAjzz8foq6IY__zi20BU&usqp=CAU" />
+            <div className="camera-icon-container">
+              <Icon name="camera retro" className="update-ava" />
+            </div>
           </div>
           <div className="personal-info-edit-container">
-            <FloatLabelInput
-              id="First and last name"
-              type="text"
-            />
+            <FloatLabelInput id="First and last name" type="text" />
             <FloatLabelInput id="Add phone number" type="tel" />
             <FloatLabelInput id="email" type="email" value={userEmail} />
             <FloatLabelInput id="Address" type="text" />
